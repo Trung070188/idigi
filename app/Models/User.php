81c389759c394166290a39964700e58ebab02e90
thuydt <thuydt@dreamlab.com.vn>
2022-06-24 14:04:39 +0700

    update datepicker

--- app/Models/User.php
+++ app/Models/User.php
@@ -125,7 +125,6 @@ WHERE p.`user_id`=?', [$this->id]);
 
     public function roles(){
         return $this->belongsToMany(Role::class,'user_role','user_id','role_id');
-//            return $this->belongsToMany('App\Role','')
     }
     public function user_roles()
     {
--- config/menu.php
+++ config/menu.php
@@ -4,45 +4,52 @@ return [
         "name" => "Dashboard",
         "icon" => "fa fa-home",
         "url" => "/xadmin/dashboard/index",
-        "group" => 1
+        "group" => 1,
+        "roles" => ['Super Administrator']
     ],
     [
         "name" => "Inventories",
         "icon" => "fa fa-archive",
         "group" => 1,
         'url' => '/xadmin/inventories/index',
+        "roles" => ['Super Administrator']
     ],
     [
         "name" => "Lesson",
         "icon" => "fa fa-book-open",
         'url' => '/xadmin/lessons/index',
+        "roles" => ['Super Administrator']
     ],
 
-     [  "name" => "Users, Roles",
+    ["name" => "Users, Roles",
         "icon" => "fa fa-users",
         "group" => 1,
         'base' => '/xadmin/users',
+        "roles" => ['Super Administrator'],
         'subs' => [
             [
                 "name" => "Manage User",
                 "icon" => "fa fa-plus",
                 "url" => "/xadmin/users/index",
                 "group" => 1,
+                "roles" => ['Super Administrator']
             ],
             [
                 "name" => "Manage Role",
                 "icon" => "fa fa-plus",
                 "url" => "/xadmin/roles/index",
                 "group" => 1,
+                "roles" => ['Super Administrator']
             ],
         ],
-         ],
+    ],
     [
 
-            "name" => "Teacher",
-            "icon" => "	fas fa-landmark",
-             "group" => 1,
-            "url" => "/xadmin/users/index_teacher",
+        "name" => "Teacher",
+        "icon" => "	fas fa-landmark",
+        "group" => 1,
+        "url" => "/xadmin/users/index_teacher",
+        "roles" => ['Super Administrator']
 
     ],
 
@@ -51,6 +58,7 @@ return [
         "name" => "School",
         "icon" => "fa fa-school",
         'url' => '/xadmin/schools/index',
+        "roles" => ['Super Administrator']
     ],
 
     [
@@ -58,12 +66,14 @@ return [
         "icon" => "fa fa-users",
         "group" => 1,
         'base' => '/xadmin/system',
+        "roles" => ['Super Administrator'],
         'subs' => [
             [
                 "name" => "File Manager",
                 "icon" => "fa fa-files-o",
                 "url" => "/xadmin/elfinder",
                 "group" => 1,
+                "roles" => ['Super Administrator']
             ],
 
         ]
@@ -72,20 +82,23 @@ return [
         "name" => "Manage devices",
         "icon" => "	fas fa-laptop",
         'url' => '/xadmin/user_devices/index',
-        'group'=>1,
+        'group' => 1,
+        "roles" => ['Super Administrator']
     ],
     [
         "name" => "Notification",
         "icon" => "fa fa-bell",
         "group" => 1,
         'url' => '/xadmin/notifications/index',
-        ],
+        "roles" => ['Super Administrator']
+    ],
 
     [
         "name" => "Download Application",
         "icon" => "fa fa-download",
         "group" => 1,
         'url' => '/xadmin/app_versions/index',
+        "roles" => ['Super Administrator']
 
     ],
 ];
