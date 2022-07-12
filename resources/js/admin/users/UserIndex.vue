d07aee5b3ac9d4e205976a9a323a93bb38c0cb18
quangtrung <quangtrung01654748714@gmail.com>
2022-07-12 16:07:15 +0700

    up

--- resources/js/admin/users/UserIndex.vue
+++ resources/js/admin/users/UserIndex.vue
@@ -26,7 +26,7 @@
                                                style="width: 400px"
                                                class="form-control " placeholder="Search ID, username, email, role..."
                                                value="">
-                                        <span class="svg-icon svg-icon-2 svg-icon-lg-1 me-0" @click="filterClear">
+                                        <span v-if="filter.keyword!==''" class="svg-icon svg-icon-2 svg-icon-lg-1 me-0" @click="filterClear">
                                             <svg type="button" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" style="margin: 3px -25px 0px;">
                                             <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
                                                         <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
