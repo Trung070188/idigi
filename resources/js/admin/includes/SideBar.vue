2e00f32902a739d8a60c4a01e68874d9d902ede2
tungnt <tungnt.blue@gmail.com>
2022-07-13 16:31:38 +0700

    D

--- resources/js/admin/includes/SideBar.vue
+++ resources/js/admin/includes/SideBar.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div style="background:#1e1e2d;" id="kt_aside" class="aside aside-hoverable" data-kt-drawer="true"
+    <div style="background:#1e1e2d;" id="kt_aside" class="aside aside-dark aside-hoverable" data-kt-drawer="true"
          data-kt-drawer-name="aside"
          data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true"
          data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start"
