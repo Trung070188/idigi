edc46b98fb2f2fed9f06d210db20b47fe72946c6
Trung <trung.nguyen@blueocean.net.vn>
2022-07-16 21:42:38 +0700

    up

--- resources/js/admin/lessons/LessonIndex.vue
+++ resources/js/admin/lessons/LessonIndex.vue
@@ -93,7 +93,7 @@
                                             <label>Creation time </label>
                                             <Daterangepicker v-model="filter.created" class="active"
                                                              placeholder="Creation date" readonly></Daterangepicker>
-                                            <span v-if="filter.created!==''" class="svg-icon svg-icon-2 svg-icon-lg-1 me-0" @click="filterClear">
+                                            <span v-if="filter.created!==''" class="svg-icon svg-icon-2 svg-icon-lg-1 me-0" @click="filterClear()">
                                             <svg type="button" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" style="float: right;margin: -32px 3px 0px;">
                                             <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
                                                         <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
