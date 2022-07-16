02cbe098ddcb7c42698ca8f2e3aaf6b1827781c0
tungnt <tungnt.blue@gmail.com>
2022-07-16 12:01:50 +0700

    D

--- resources/views/admin/layouts/vue.blade.php
+++ resources/views/admin/layouts/vue.blade.php
@@ -15,7 +15,12 @@
 
 		<link rel="shortcut icon" href="/themes/admin/assets/media/logos/favicon.ico" />
 
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&amp;display=swap" rel="stylesheet">
+        <!--<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&amp;display=swap" rel="stylesheet">-->
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+
         <link href="/assets/js/daterangepicker/daterangepicker.css" rel="stylesheet" type="text/css" />
         <link href="/assets/js/daterangepicker/daterangepicker.css" rel="stylesheet" type="text/css" />
         <link href="/assets/plugins/ckeditor/plugins/codesnippet/lib/highlight/styles/default.css" rel="stylesheet"/>
@@ -28,7 +33,7 @@
         <link href="{{ asset('/assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
         <!--<link href="{{ asset('themes/admin/assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />-->
         <link rel="stylesheet" href="/assets/css/jquery-ui.css">
-        <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="/assets/css/style.css?v=1">
         <style>
             .btn-trash i{
                 font-size: 1.3rem;
