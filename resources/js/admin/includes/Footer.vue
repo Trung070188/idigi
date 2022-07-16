8bfc2622e9d91f3464a45095d701245693f96427
Trung <trung.nguyen@blueocean.net.vn>
2022-07-16 18:00:15 +0700

    up

--- resources/js/admin/includes/Footer.vue
+++ resources/js/admin/includes/Footer.vue
@@ -3,7 +3,7 @@
         <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
             <div class="text-dark order-2 order-md-1">
                 <span class="text-muted fw-bold me-1">©2022</span>
-                <a href="https://ismart.edu.vn/" target="_blank" class="text-gray-800 text-hover-primary">iSMART Education.</a>
+                <a href="https://ismart.edu.vn/" target="_blank" class="text-gray-800 text-hover-primary">All rights reserved | iSMART Education</a>
             </div>
         </div>
     </div>
