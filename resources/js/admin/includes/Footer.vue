fda3b02f4cb1d556d2d67b902627c1138888697a
Trung <trung.nguyen@blueocean.net.vn>
2022-07-17 11:48:57 +0700

    up

--- resources/js/admin/includes/Footer.vue
+++ resources/js/admin/includes/Footer.vue
@@ -2,8 +2,8 @@
     <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
         <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
             <div class="text-dark order-2 order-md-1">
-                <span class="text-muted fw-bold me-1"> Copyright ©2022</span>
-                <a href="https://ismart.edu.vn/" target="_blank" class="text-gray-800 text-hover-primary">All rights reserved | iSMART Education</a>
+                <span class="text-muted fw-bold me-1"> Copyright © 2022</span>
+                <a href="https://ismart.edu.vn/" target="_blank" class="text-gray-800 text-hover-primary" >All rights reserved | iSMART Education</a>
             </div>
         </div>
     </div>
