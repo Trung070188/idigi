8f1f80075709b129a563b6ed4e25a512c76c2a4d
Trung <trung.nguyen@blueocean.net.vn>
2022-07-19 21:01:39 +0700

    up

--- resources/js/admin/lessons/LessonIndex.vue
+++ resources/js/admin/lessons/LessonIndex.vue
@@ -53,7 +53,7 @@
                                 <div class="card-title">
                                         <div class="d-flex align-items-center position-relative my-1">
                                             <!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
-                                            <span class="svg-icon svg-icon-1 position-absolute">
+                                            <span class="svg-icon svg-icon-1 position-absolute ms-6">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                                     <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="black"></rect>
                                                     <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="black"></path>
@@ -75,20 +75,20 @@
 
                                         <button type="button" style="margin-left: 10px"
                                                 @click="isShowFilter = !isShowFilter"
-                                                class="btn btn-primary" v-if="isShowFilter"> Close Adventure search
+                                                class="btn btn-primary" v-if="isShowFilter"> Close Advanced Search
                                             <i style="margin-left: 5px" class="fas fa-times"></i>
 
                                         </button>
                                         <button type="button" style="margin-left: 10px"
                                                 @click="isShowFilter = !isShowFilter"
-                                                class="btn btn-primary" v-if="!isShowFilter"> Adventure search
+                                                class="btn btn-primary" v-if="!isShowFilter"> Advanced Search
                                             <i class="fa fa-filter" v-if="!isShowFilter" aria-hidden="true"></i>
                                         </button>
                                     </div>
                                      <div class="d-flex justify-content-end align-items-center d-none" data-kt-customer-table-toolbar="selected" v-if="permissions['011'] && lessonIds!=''">
 												<div class="fw-bolder me-5">
-												<span class="me-2" data-kt-customer-table-select="selected_count"></span>{{ lessonIds.length }} Lesson Selected</div>
-												<button @click="openModal()" type="button" class="btn btn-primary" data-kt-customer-table-select="delete_selected">Download Lesson</button>
+												<span class="me-2" data-kt-customer-table-select="selected_count"></span>{{ lessonIds.length }} Selected</div>
+												<button @click="openModal()" type="button" class="btn btn-danger" data-kt-customer-table-select="delete_selected">Download Selected</button>
 											</div>
 
                                 </div>
@@ -179,7 +179,7 @@
 
                                         <div>
                                             <!-- {{ lessonIds.length }} lesson selected -->
-                                            <a href="javascript:;" @click="removeAll" style="color: red; margin-left: 10px">clear all</a>
+                                            <!-- <a href="javascript:;" @click="removeAll" style="color: red; margin-left: 10px">clear all</a> -->
                                         </div>
                                     </template>
 
