432ca89aeb84b8c42079cc93a3b0381a1795c5cb
thuydt <thuy.dinh@dreamlab.edu.vn>
2022-08-16 20:27:05 +0700

    Enable queue:work

--- conf/queue.sh
+++ conf/queue.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-QUEUE_ENABLED=$(php /var/www/artisan AppEnv QUEUE_ENABLED)
-
-if [[ "$QUEUE_ENABLED" == "1" ]]; then
-    while :
-    do
-      echo "queue:work started"
-      php /var/www/artisan queue:work --tries=3 --timeout=120 2>&1
-      sleep 5
-    done
-else
-    echo "queue:work is disabled"
-fi
-
+while :
+do
+  echo "queue:work started"
+  php /var/www/artisan queue:work --tries=3 --timeout=120 2>&1
+  sleep 5
+done
--- conf/schedule.sh
+++ conf/schedule.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-  echo 'schedule:run started'
+    echo 'schedule:run started'
 	php /var/www/artisan schedule:run
 	sleep 60
 done
