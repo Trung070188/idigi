a74517ae4255d9aa4f41d601d4f679b41464c4fd
quangtrung <quangtrung01654748714@gmail.com>
2022-08-16 11:26:02 +0700

    up

--- resources/js/admin/includes/NavBar.vue
+++ resources/js/admin/includes/NavBar.vue
@@ -8,12 +8,12 @@
                 <div class="header-menu align-items-stretch" data-kt-drawer="true" data-kt-drawer-name="header-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_header_menu_mobile_toggle" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_body', lg: '#kt_header_nav'}">
                     <div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true" >
 
-                        <template v-if="roleName=='School Admin' || 'Teacher'">
+                        <template >
                             <div data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" class="menu-item menu-lg-down-accordion me-lg-1" style="margin: 12px 0px 0px">
                                 <span class="menu-link py-3">
 
                            <div class="badge badge-lg badge-light-primary mb-15">
-                               <div class="d-flex align-items-center flex-wrap">
+                               <div class="d-flex align-items-center flex-wrap" v-if="roleName=='School Admin' || roleName=='Teacher' ">
                                    <div v-text="'Trường '+ schoolName " ></div>
 
 
