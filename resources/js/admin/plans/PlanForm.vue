c08c971b8ed54b5dfb0837599a9c04956c1a9a65
quangtrung <quangtrung01654748714@gmail.com>
2022-09-05 15:14:52 +0700

    up

--- resources/js/admin/plans/PlanForm.vue
+++ resources/js/admin/plans/PlanForm.vue
@@ -1,61 +1,114 @@
 <template>
-    <div class="container-fluid">
-        <ActionBar type="form" @save="save()"
-                   :code="entry.id"
-                   backUrl="/xadmin/plans/index"
-                   title="PlanForm"/>
+    <div class="container-fluid" >
+        <ActionBar type="index"
+                   :breadcrumbs="breadcrumbs" title ="Create new plan" />
         <div class="row">
             <div class="col-lg-12">
                 <div class="card card-custom card-stretch gutter-b">
+                    <div class="card-header border-0 pt-6" style="margin:0px 0px -35px">
+                        <div class="card-title"></div>
+                        <div class="card-toolbar">
+
+                        </div>
+
+                    </div>
+
                     <div class="card-body d-flex flex-column" >
                         <div class="row">
                             <div class="col-lg-12">
                                 <input v-model="entry.id" type="hidden" name="id" value="">
-                                                                    <div class="form-group">
-                                        <label>Name</label>
-                                        <input id="f_name" v-model="entry.name" name="name" class="form-control"
-                                               placeholder="name" >
-                                        <error-label for="f_name" :errors="errors.name"></error-label>
+                                <div class="row">
+                                    <div class="form-group col-lg-8">
+                                        <label>Plan name <span class="text-danger">*</span></label>
+                                        <input  v-model="entry.name"  class="form-control"
+                                                placeholder="Enter the name of plan" >
+                                        <error-label for="f_school_name" ></error-label>
 
                                     </div>
-                                                                    <div class="form-group">
-                                        <label>Status</label>
-                                        <input id="f_status" v-model="entry.status" name="name" class="form-control"
-                                               placeholder="status" >
-                                        <error-label for="f_status" :errors="errors.status"></error-label>
+
+                                    <div class="form-group col-lg-4">
+                                        <label> Assign to IT <span class="text-danger">*</span></label>
+                                        <select   class="form-control form-select"
+                                        >
+                                            <option></option>
+                                        </select>
+                                        <error-label  ></error-label>
 
                                     </div>
-                                                                    <div class="form-group">
-                                        <label>Deployed</label>
-                                        <input id="f_Deployed" v-model="entry.Deployed" name="name" class="form-control"
-                                               placeholder="Deployed" >
-                                        <error-label for="f_Deployed" :errors="errors.Deployed"></error-label>
+
+                                </div>
+                                <div class="row">
+                                    <div class="form-group col-lg-8">
+                                        <label>Plan description  <span class="text-danger">*</span> </label>
+                                        <input   class="form-control"
+                                                placeholder="Enter the description" >
+                                        <error-label for="f_school_name" :errors="errors.school_phone"></error-label>
 
                                     </div>
-                                                                    <div class="form-group">
-                                        <label>Created By</label>
-                                        <input id="f_created_by" v-model="entry.created_by" name="name" class="form-control"
-                                               placeholder="created_by" >
-                                        <error-label for="f_created_by" :errors="errors.created_by"></error-label>
+
+                                    <div class="form-group col-lg-4">
+                                        <label>Due date  <span class="text-danger">*</span></label>
+                                        <Datepicker />
+                                        <error-label for="f_title" ></error-label>
+
 
                                     </div>
-                                                                    <div class="form-group">
-                                        <label>Due At</label>
-                                        <input id="f_due_at" v-model="entry.due_at" name="name" class="form-control"
-                                               placeholder="due_at" >
-                                        <error-label for="f_due_at" :errors="errors.due_at"></error-label>
 
+
+                                </div>
+                                <div class="row">
+                                    <div class="form-group col-lg-8">
+                                        <label>Devices <span class="text-danger">*</span></label>
+                                        <div class="card-header  border border-dashed border-gray-300">
+                                            <!--begin::Card title-->
+                                            <div class="card-title">
+                                                <h2 class="fw-bolder"></h2>
+                                            </div>
+                                            <!--end::Card title-->
+                                            <!--begin::Card toolbar-->
+                                            <div class="card-toolbar">
+                                              <button class="btn btn-primary">View devices</button>
+                                                <button class="btn btn-primary" style="margin: 0px 15px 0px">Add a device</button>
+                                                <button class="btn btn-primary">Import devices</button>
+                                            </div>
+                                            <!--end::Card toolbar-->
+                                        </div>
+
+                                        </div>
                                     </div>
-                                
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group col-lg-8">
+                                        <label>Lesson package <span class="text-danger">*</span></label>
+                                        <div class="card-header  border border-dashed border-gray-300">
+                                            <!--begin::Card title-->
+                                            <div class="card-title">
+                                                <h2 class="fw-bolder"></h2>
+                                            </div>
+                                            <!--end::Card title-->
+                                            <!--begin::Card toolbar-->
+                                            <div class="card-toolbar">
+                                                <button class="btn btn-primary">Download package</button>
+                                                <button class="btn btn-primary" style="margin: 0px 15px 0px">View lessons</button>
+                                                <button class="btn btn-primary">Add lesson</button>
+                                            </div>
+                                            <!--end::Card toolbar-->
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
+
                         </div>
-                        <hr>
-                        <div >
+                        <hr style="margin-top: 5px;">
+                        <div style="margin: 13px 25px 13px">
                             <button type="reset" @click="save()" class="btn btn-primary mr-2">Save</button>
                             <button type="reset" @click="backIndex()" class="btn btn-secondary">Cancel</button>
                         </div>
                     </div>
                 </div>
+
+
             </div>
         </div>
     </div>
@@ -71,6 +124,15 @@
         components: {ActionBar},
         data() {
             return {
+                breadcrumbs: [
+                    {
+                        title: 'Manage plans',
+                        url:'/xadmin/plans/index'
+                    },
+                    {
+                        title: 'Create new plan'
+                    },
+                ],
                 entry: $json.entry || {},
                 isLoading: false,
                 errors: {}
