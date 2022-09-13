7d0d4e0572168b3ab9bec213159b843217ed0e43
Trung <trung.nguyen@blueocean.net.vn>
2022-09-13 15:22:21 +0700

    up

--- resources/js/admin/schools/LicenseExpired.vue
+++ resources/js/admin/schools/LicenseExpired.vue
@@ -2,8 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <div style="text-align:center">
-                    <h1>Your session is about to expire due to the license. <br>To establish a new session, please contact us to renew your license.</h1>
+            <a href="/" >
+                <img alt="Logo" src="/images/Logo_iDIGI_black 1.png" class="h-100px logo" style="display: block;margin-left: auto;margin-right: auto;margin-bottom: 52px;">
+            </a>                
+            <div style="text-align:center">
+                    <h4>Your session is about to expire due to the license. <br>To establish a new session, please contact us to renew your license.</h4>
                 </div>
             </div>
         </div>
