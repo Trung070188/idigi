a4dda0b419c9e1bd1233450eabf746bbf8091da6
thuydt <thuy.dinh@dreamlab.edu.vn>
2022-09-27 22:02:18 +0700

    xoa data cap 2

---
+++ app/Console/Commands/DeleteDataSec.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Helpers\PhpDoc;
+use App\Models\Course;
+use App\Models\DownloadLessonFile;
+use App\Models\DownloadLessonLog;
+use App\Models\File;
+use App\Models\Inventory;
+use App\Models\Lesson;
+use App\Models\LessonInventory;
+use App\Models\Product;
+use App\Models\Unit;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class DeleteDataSec extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'delete-data-sec';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Xóa file cap2';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $data = Inventory::where('level', 'sec')->get();
+
+
+        foreach ($data as $item){
+            if($item->path){
+                if(is_file($item->path)){
+                    try {
+                        unlink($item->path);
+
+
+                    }
+                    catch (\Exception $e) {
+                        echo $e->getMessage();
+                    }
+                }
+
+                echo 'Delete file in record: '. $item->id.PHP_EOL;
+            }
+
+        }
+
+    }
+
+
+}
