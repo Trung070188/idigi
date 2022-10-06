60991e5f7a77508f323f34487ffa7a35fa0d5a4c
quangtrung <quangtrung01654748714@gmail.com>
2022-10-06 15:29:05 +0700

    update

--- resources/js/admin/notifications/NotificationIndex.vue
+++ resources/js/admin/notifications/NotificationIndex.vue
@@ -209,7 +209,8 @@
                                 <td class="" v-text="entry.role"></td>
                                 <td class="" v-if="entry.title==='Yêu cầu xóa thiết bị'">Yêu cầu xóa thiết bị</td>
                                 <td class="" v-if="entry.title==='Yêu cầu cấp quyền'">Yêu cầu cấp quyền</td>
-                                <td class="" v-if="entry.title==='Yêu cầu xóa thiết bị'">
+                                <td class="" v-if="entry.title==='File download plan'">File download plan</td>
+                                <td class="" >
                                     <a href="list.html#" class="btn btn-light btn-active-light-primary btn-sm"
                                        data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
                                         <!--begin::Svg Icon | path: icons/duotune/arrows/arr072.svg-->
@@ -239,35 +240,35 @@
                                     <!--end::Menu-->
                                 </td>
 
-                                <td class="" v-if="entry.title==='Yêu cầu cấp quyền'">
-                                    <a href="list.html#" class="btn btn-light btn-active-light-primary btn-sm"
-                                       data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
-                                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr072.svg-->
-                                        <span class="svg-icon svg-icon-5 m-0">
-															<svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                                 height="24" viewBox="0 0 24 24" fill="none">
-																<path
-                                                                    d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z"
-                                                                    fill="black"/>
-															</svg>
-														</span>
-                                        <!--end::Svg Icon--></a>
-                                    <!--begin::Menu-->
-                                    <div
-                                        class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4"
-                                        data-kt-menu="true">
-                                        <!--begin::Menu item-->
-                                        <div class="menu-item px-3" v-if="permissions['026']">
-                                            <a :href="entry.url" class="menu-link px-3">View Detail</a>
-                                        </div>
-                                        <div class="menu-item px-3" v-if="permissions['026']">
-                                            <a @click="removeNotification(entry.id)" data-kt-subscriptions-table-filter="delete_row"
-                                               class="menu-link text-danger px-3">Delete</a>
-                                        </div>
-                                        <!--end::Menu item-->
-                                    </div>
-                                    <!--end::Menu-->
-                                </td>
+<!--                                <td class="" v-if="entry.title==='Yêu cầu cấp quyền'">-->
+<!--                                    <a href="list.html#" class="btn btn-light btn-active-light-primary btn-sm"-->
+<!--                                       data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions-->
+<!--                                        &lt;!&ndash;begin::Svg Icon | path: icons/duotune/arrows/arr072.svg&ndash;&gt;-->
+<!--                                        <span class="svg-icon svg-icon-5 m-0">-->
+<!--															<svg xmlns="http://www.w3.org/2000/svg" width="24"-->
+<!--                                                                 height="24" viewBox="0 0 24 24" fill="none">-->
+<!--																<path-->
+<!--                                                                    d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z"-->
+<!--                                                                    fill="black"/>-->
+<!--															</svg>-->
+<!--														</span>-->
+<!--                                        &lt;!&ndash;end::Svg Icon&ndash;&gt;</a>-->
+<!--                                    &lt;!&ndash;begin::Menu&ndash;&gt;-->
+<!--                                    <div-->
+<!--                                        class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4"-->
+<!--                                        data-kt-menu="true">-->
+<!--                                        &lt;!&ndash;begin::Menu item&ndash;&gt;-->
+<!--                                        <div class="menu-item px-3" v-if="permissions['026']">-->
+<!--                                            <a :href="entry.url" class="menu-link px-3">View Detail</a>-->
+<!--                                        </div>-->
+<!--                                        <div class="menu-item px-3" v-if="permissions['026']">-->
+<!--                                            <a @click="removeNotification(entry.id)" data-kt-subscriptions-table-filter="delete_row"-->
+<!--                                               class="menu-link text-danger px-3">Delete</a>-->
+<!--                                        </div>-->
+<!--                                        &lt;!&ndash;end::Menu item&ndash;&gt;-->
+<!--                                    </div>-->
+<!--                                    &lt;!&ndash;end::Menu&ndash;&gt;-->
+<!--                                </td>-->
 
 
                             </tr>
