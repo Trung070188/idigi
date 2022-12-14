b29e0025b658a7a01b2916fea71da6bb5f5307b3
quangtrung <quangtrung01654748714@gmail.com>
2022-12-14 11:26:54 +0700

    update

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -37,7 +37,7 @@ class Permission extends BaseModel
     }
     public function permissionDetails()
     {
-        return $this->hasMany(PermissionDetail::class,'permission_id')->orderBy('order','ASC');
+        return $this->hasMany(PermissionDetail::class,'permission_id')->orderBy('order','desc');
     }
     public function groupPermission(){
         return $this->belongsTo(GroupPermission::class, 'group_permission_id');
