e22a58aa20f42c5cd63142dc9d656b61edccd8b0
thuydt <thuy.dinh@dreamlab.edu.vn>
2023-03-13 08:55:47 +0700

    fix course

--- app/Http/Controllers/Admin/InventoriesController.php
+++ app/Http/Controllers/Admin/InventoriesController.php
@@ -176,7 +176,7 @@ class InventoriesController extends AdminBaseController
         }
 
 
-        $v = Validator::make($data, $rules, ['lessonId.required' => 'The lesson is required.']);
+        $v = Validator::make($data, $rules, ['lessonId.required' => 'The lesson field is required.']);
 
         if ($v->fails()) {
             return [
