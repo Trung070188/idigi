f2d5afc81f6ea9538ba1a40205cd895a68f693f9
thuydt <thuy.dinh@dreamlab.edu.vn>
2023-03-14 13:12:27 +0700

    update

--- app/Http/Controllers/Api/LessonController.php
+++ app/Http/Controllers/Api/LessonController.php
@@ -61,7 +61,7 @@ class LessonController extends Controller
             }
         }
 
-        $query = Lesson::query()->with(['user_units'])
+        $query = Lesson::query()->with(['user_units', 'invetories', 'unit1', 'unit1.course'])
             ->orderBy('position', 'ASC')
             ->orderBy('id', 'ASC');
 
@@ -73,6 +73,42 @@ class LessonController extends Controller
         $data = [];
 
         foreach ($lessons as $lesson){
+            $inventoryData = [];
+
+            if($lesson->inventories){
+                foreach ($lesson->inventories as $inventory) {
+                    $pathArr = explode('/', $inventory->virtual_path);
+                    $inventoryData[] = [
+                        "idSublesson" => $inventory->id,
+                        "pathIcon" => "",
+                        "name" => $inventory->name,
+                        "time" => "",
+                        "type" => $inventory->type,
+                        "link" => $pathArr[count($pathArr) - 1],
+                        "full_link" => url('/api/download/inventory/' . $inventory->id)
+                    ];
+                }
+                $lessonNameArr = explode(':', $lesson->name);
+                $structure = [
+                    "idSubject" => @$lesson->unit1->course->subject == 'Science' ? 1 : 0,
+                    "codeSubject" => @$lesson->unit1->course->subject,
+                    "nameSubject" => 'iSMART ' .@$lesson->unit1->course->subject,
+                    "grade" => @$lesson->unit1->course->grade,
+                    "idUnit" => @$lesson->unit1->position,
+                    "titleUnit" => @$lesson->unit1->unit_name,
+                    "nameUnit" => @$lesson->unit1->unit_name,
+                    "idLesson" => $lesson->id,
+                    "codeLesson" => $lessonNameArr[0],
+                    "titleLesson" => $lesson->name,
+                    "nameLesson" => $lesson->name,
+                    "subLessons" => $inventoryData,
+
+                ];
+
+                $lesson->structure = json_encode($structure);
+                $lesson->save();
+            }
+
             if($lesson->structure){
                 $data[] = json_decode($lesson->structure, true);
             }
